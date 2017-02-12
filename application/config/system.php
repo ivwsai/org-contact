2e587e68416632c4679b99465b870b2f4f041bfb
DC Liang <dcliang@DCdeMacBook-Pro.local>
2017-02-12 19:34:48 +0800

    Change 配置修改“ ”

--- application/config/system.php
+++ application/config/system.php
@@ -3,5 +3,5 @@ return array(
     'site_version' => '1.0',                  //工程名称，主要用于js/css缓存的版本控制
     'theme' => '/static/themes/default/',     //项目皮肤主要有 /static/default/(css|images|(js/user|etc..))
     'js_lib' => '/static/lib/',
-    'base_url' => "http://127.0.0.1",  //站点访问地址eg:http://localhost or localhost/oa/(兼容旧版本使用)
+    'base_url' => "http://".$_SERVER['HTTP_HOST'],  //站点访问地址eg:http://localhost or localhost/oa/(兼容旧版本使用)
 );
