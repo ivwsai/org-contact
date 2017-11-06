6a09e31a67d29f61102e3002fa0bdd12e56601c4
houxh <houxuehua49@gmail.com>
2017-11-06 19:11:32 +0800

    数据库配置

--- application/config/database.php
+++ application/config/database.php
@@ -1,8 +1,8 @@
 <?php defined('SYSPATH') or die('No direct access allowed.');
 return array(
     'default' => array(
-        'hostspec' => '127.0.0.1',
-        'username' => 'root',
+        'hostspec' => '10.251.43.254',
+        'username' => 'oa',
         'password' => '123456',
         'port' => 3306,
         'database' => 'oa',
