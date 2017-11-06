1c3cd09bc61e3074a208764af6ebfcd5711a725c
houxh <houxuehua49@gmail.com>
2017-11-06 19:47:06 +0800

    启动脚本

--- run.sh
+++ run.sh
@@ -6,14 +6,18 @@ docker_dir="$(pwd)/docker"
 app_data="$(pwd)/group"
 log_dir="${app_data}/logs"
 app_log_dir="$(pwd)/application/logs"
+app_log_dir2="$(pwd)/application/tempfiles/logs"
 
 name="goubuli.mobi"
 image="php_dev"
 
-    
 mkdir -p ${log_dir}
+
 mkdir -p ${app_log_dir}
 chmod -R 0777 ${app_log_dir}
+
+mkdir -p ${app_log_dir2}
+chmod -R 0777 ${app_log_dir2}
 
 is_running() {
     return $(docker ps -a |grep $1 | wc -l)
@@ -23,7 +27,7 @@ start() {
     is_running=$(docker ps -a |grep ${name} | wc -l)
     if [ ${is_running} == 0 ]; then
         echo "start ${name}..."
-        docker run -d --restart=always --net=host --name ${name} -v "${docker_dir}/php.ini":/etc/php5/fpm/php.ini -v "${docker_dir}/group_www.conf":/etc/php5/fpm/pool.d/www.conf -v "${curr_dir}":/data/wwwroot/org -v "${log_dir}/php5":/var/log/php5 ${image} /usr/sbin/php5-fpm -R -c /etc/php5/fpm
+        docker run -d --restart=always --net=host --name ${name} -v "${docker_dir}/php.ini":/etc/php5/fpm/php.ini -v "${docker_dir}/group_www.conf":/etc/php5/fpm/pool.d/www.conf -v "${curr_dir}":/data/wwwroot/goubuli.mobi -v "${log_dir}/php5":/var/log/php5 ${image} /usr/sbin/php5-fpm -R -c /etc/php5/fpm
     else
         echo "restart ${name}..."
         docker restart ${name}
