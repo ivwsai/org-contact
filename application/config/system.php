ec22cc1d2f68294d753a3ccb0758d0485aac818d
houxh <houxuehua49@gmail.com>
2017-11-07 11:09:21 +0800

    兼容https

--- application/config/system.php
+++ application/config/system.php
@@ -1,7 +1,8 @@
 <?php defined('SYSPATH') or die('No direct access');
+$protocol = $_SERVER['HTTPS'] == "on" ? "https://" : "http://";
 return array(
     'site_version' => '1.0',                  //工程名称，主要用于js/css缓存的版本控制
     'theme' => '/static/themes/default/',     //项目皮肤主要有 /static/default/(css|images|(js/user|etc..))
     'js_lib' => '/static/lib/',
-    'base_url' => "http://".$_SERVER['HTTP_HOST'],  //站点访问地址eg:http://localhost or localhost/oa/(兼容旧版本使用)
+    'base_url' => $protocol.$_SERVER['HTTP_HOST'], 
 );
